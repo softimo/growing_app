942bea0fb2771ff2679f554ce6649d35d526d956
softimo <softimosolutions@gmail.com>
2022-06-29 16:56:47 -0300

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/main.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
